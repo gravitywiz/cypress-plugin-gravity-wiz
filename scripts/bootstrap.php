92880ae2171e3e4b64108c2099c0b1fe259c73df
Clay Griffiths <clay@claygriffiths.com>
2021-02-16 22:34:02 -0600

    Fixed callback name in add_filter for gwiz_legacy_check

--- scripts/bootstrap.php
+++ scripts/bootstrap.php
@@ -59,7 +59,7 @@ function enable_form_ajax( $args ) {
 }
 
 // Add legacy filter to enable legacy markup for acceptance tests
-add_filter( 'gform_enable_legacy_markup', 'legacy_check' );
+add_filter( 'gform_enable_legacy_markup', 'gwiz_legacy_check' );
 function gwiz_legacy_check( $value ) {
 	if ( rgget( 'enable_legacy' ) ) {
 		return boolval( rgget( 'enable_legacy' ) );
