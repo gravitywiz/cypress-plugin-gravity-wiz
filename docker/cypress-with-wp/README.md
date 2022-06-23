eedb1300fba10f665ca4aabd8f6fd3d424151202
Clay Griffiths <clay@claygriffiths.com>
2022-06-23 17:12:04 -0500

    Rename Docker folder to `cypress-with-wp`, add build script to allow specifying Cypress version, use Cypress version as the image tag

---
+++ docker/cypress-with-wp/README.md
@@ -0,0 +1,5 @@
+## Build & Publish
+
+```bash
+./build.sh "10.2.0"
+```
---
+++ docker/cypress-with-wp/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+CYPRESS_VERSION=$1
+
+MAJOR=`echo $CYPRESS_VERSION | cut -d. -f1`
+#MINOR=`echo $CYPRESS_VERSION | cut -d. -f2`
+
+test -n "$CYPRESS_VERSION" || { echo "CYPRESS_VERSION not set"; exit 1; }
+
+docker build \
+  --platform linux/amd64 \
+  --build-arg CYPRESS_VERSION=$CYPRESS_VERSION \
+  --squash \
+  . \
+  --tag "gravitywiz/cypress-with-wp:$CYPRESS_VERSION" \
+  --tag "gravitywiz/cypress-with-wp:$MAJOR"
+
+docker push gravitywiz/cypress-with-wp:$CYPRESS_VERSION
+docker push gravitywiz/cypress-with-wp:$MAJOR
