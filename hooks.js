b0ba1aef59003b641116e6f85cdef3b6bd3dc84b
Clay Griffiths <clay@claygriffiths.com>
2023-02-13 12:39:06 -0600

    ~ Updated `hooks.js` to better ignore ResizeObserver error

--- hooks.js
+++ hooks.js
@@ -1,16 +1,13 @@
 before(() => {
+    /* returning false here prevents Cypress from failing the test */
+    Cypress.on(
+        'uncaught:exception',
+        // @ts-ignore
+        err => !err.message.includes('ResizeObserver loop limit exceeded'),
+    )
+
     Cypress.on( 'uncaught:exception', ( err ) => {
-        /* returning false here prevents Cypress from failing the test */
-
-        // Not sure why this error is occurring but since Cypress fails on any application-side error, we need to
-        // prevent this particular error from failing the entire test.
-        const resizeObserverLoopErrRe = /^(ResizeObserver loop limit exceeded)/;
-
-        if ( resizeObserverLoopErrRe.test( err.message ) ) {
-            return false;
-
-            /* seeing this error occasionally after logging in */
-        } else if (
+        if (
             err.message.indexOf(
                 "Cannot read properties of undefined (reading 'replace')"
             ) !== -1
