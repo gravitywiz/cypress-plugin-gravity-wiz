e7984ee70cd683e6326a09b6160f95c7b03fe7b3
Clay Griffiths <clay@claygriffiths.com>
2023-07-15 18:58:42 -0500

    WIP

--- docker/cypress-with-wp/build.sh
+++ docker/cypress-with-wp/build.sh
@@ -8,11 +8,10 @@ test -n "$CYPRESS_VERSION" || { echo "CYPRESS_VERSION not set"; exit 1; }
 docker build \
   --platform linux/amd64 \
   --build-arg CYPRESS_VERSION=$CYPRESS_VERSION \
-  --squash \
   . \
-  --tag "gravitywiz/cypress-with-wp:$CYPRESS_VERSION" \
-  --tag "gravitywiz/cypress-with-wp:$MAJOR" \
-  --tag "gravitywiz/cypress-with-wp:latest" # Remove me if building an older version
+  --tag "gravitywiz/cypress-with-wp-php8.1:$CYPRESS_VERSION" \
+  --tag "gravitywiz/cypress-with-wp-php8.1:$MAJOR" \
+  --tag "gravitywiz/cypress-with-wp-php8.1:latest" # Remove me if building an older version
 
 docker push gravitywiz/cypress-with-wp:$CYPRESS_VERSION
 docker push gravitywiz/cypress-with-wp:$MAJOR
